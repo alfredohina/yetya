5cfaf625d2c2d5675b877eb714837715b965516c
Gabriel Cueva <gabrielcueva_10@hotmail.com>
2018-12-18 20:27:09 +0100

    cuadros OK

--- app.js
+++ app.js
@@ -35,22 +35,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-
-app.use(session({
-  secret: "basic-auth-secret",
-  cookie: {
-    maxAge: 60000
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
-  })
-}));
-app.use(flash());
-
-
-
 // Enable authentication using session + passport
 app.use(session({
   secret: 'irongenerator',
