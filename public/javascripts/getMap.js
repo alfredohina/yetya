4c11a69f0e7e78c27256acf440f76eab7d14beda
Gabriel Cueva <gabrielcueva_10@hotmail.com>
2018-12-20 19:41:58 +0100

    Eventos funcionando y filtrando a unnn kilometrooo todos los datos

--- public/javascripts/getMap.js
+++ public/javascripts/getMap.js
@@ -6,7 +6,7 @@ const geolocateMe = () => {
           resolve({
             lat: position.coords.latitude,
             lng: position.coords.longitude
-          })
+          });
         },
         () => {
           reject("Error in the geolocation service.");
@@ -18,7 +18,7 @@ const geolocateMe = () => {
   });
 };
 
-const addMarker = (titulo, posicion, map,icon,infowindow) => {
+const addMarker = (titulo, posicion, map, icon, infowindow) => {
   let marker = new google.maps.Marker({
     position: posicion,
     map,
@@ -28,34 +28,53 @@ const addMarker = (titulo, posicion, map,icon,infowindow) => {
   marker.addListener("click", function() {
     infowindow.open(map, marker);
   });
-   return marker
+  return marker;
 };
 
 let infowindow;
 
-const loadData = map => {
-  events.forEach(event => {
-    //console.log(events.name)
-    let infowindow = new google.maps.InfoWindow({
-      content:
-        "<div><strong>" +
-        event.name +
-        "</strong><br>" +
-        "Description: " +
-        event.description + "</br>" +
-        `<a href="/events/${event._id}" class="btn btn-outline-success"><i class="fas fa-eye"></i> View event</a>`
+function calcDistance(p1, p2) {
+  p1 = new google.maps.LatLng(p1.lat, p1.lng);
+    p2 = new google.maps.LatLng(p2[0], p2[1]);
+  return (
+    google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000
+  ).toFixed(2);
+}
 
-      ,maxWidth: 400
+const loadData = map => {
+  geolocateMe().then(center => {
+    //const myLoc = new google.maps.LatLng(center.lat, center.lng);
+    
+    events.forEach(event => {
+      let distance = calcDistance(center, event.location.coordinates);
+
+      if (distance <= 1) {
+        let infowindow = new google.maps.InfoWindow({
+          content:
+            "<div><strong>" +
+            event.name +
+            "</strong><br>" +
+            "Description: " +
+            event.description +
+            "</br>" +
+            `<a href="/events/${
+              event._id
+            }" class="btn btn-outline-success"><i class="fas fa-eye"></i> View event</a>`,
+
+          maxWidth: 400
+        });
+        addMarker(
+          event.name,
+          {
+            lat: event.location.coordinates[0],
+            lng: event.location.coordinates[1]
+          },
+          map,
+          "https://res.cloudinary.com/drlexgkiu/image/upload/v1545159229/yetyamaps.png",
+          infowindow
+        );
+      }
     });
-    addMarker(
-      event.name,
-      {
-        lat: event.location.coordinates[0],
-        lng: event.location.coordinates[1]
-      },
-      map,
-       'https://res.cloudinary.com/drlexgkiu/image/upload/v1545159229/yetyamaps.png',
-      infowindow
-    );
-  });
+  })
+  .catch(err=>console.log(err))
 };
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,63 +1,56 @@
+let map;
+document.addEventListener("DOMContentLoaded", () => {
+  // const map =  'hola' //document.getElementById('map')
 
-const map = new google.maps.Map(
-  document.getElementById('map'), {
+
+   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
     center: {
       lat: 42.4197351,
       lng: -3.7040427
     }
-  }
-);
+  });
 
-//addmaker('A ver si sale', {lat:40.4045385,lng: -3.6988189}, map)
+  //addmaker('A ver si sale', {lat:40.4045385,lng: -3.6988189}, map)
 
-loadData(map);
+  loadData(map);
 
-// google.maps.event.addListener(map, 'click', 
-// function(event){
-//   //addMarker('Evento Nuevo',event.latLng,map)
-//   console.log(`Esta son las coordenadas ${event.latLng}`)
-//   new google.maps.Marker({
-//     position:event.latLng ,
-//     map,
-//     title: 'Evento Nuevo, aqui se lia'
-//   })
-// })
+  // google.maps.event.addListener(map, 'click',
+  // function(event){
+  //   //addMarker('Evento Nuevo',event.latLng,map)
+  //   console.log(`Esta son las coordenadas ${event.latLng}`)
+  //   new google.maps.Marker({
+  //     position:event.latLng ,
+  //     map,
+  //     title: 'Evento Nuevo, aqui se lia'
+  //   })
+  // })
 
-
-
-
-geolocateMe()
-  .then(center => {
-    //console.log(`esto es Longitud ${center.lng} y esto es lat ${center.lat}`)
-    latitud = center.lat;
-    longitud = center.lng;
-    let infowindow = new google.maps.InfoWindow({
-      content: "Holita",
-      maxWidth: 400
+  geolocateMe()
+    .then(c => {
+      latitud = c.lat;
+      longitud = c.lng;
+      let infowindow = new google.maps.InfoWindow({
+        content: "Holita",
+        maxWidth: 400
+      });
+      addMarker("Tu", c, map, infowindow);
+      getApiData(latitud, longitud);
+      map.setCenter(c);
+      center = c;
+    })
+    .catch(() => {
+      console.log("Aqui catch geolocateme");
+      geolocateMe();
     });
-    addMarker('Tu', center, map, infowindow);
-    getApiData(latitud,longitud)
-    map.setCenter(center)
-  })
-  .catch(() => { 
-    console.log('Aqui catch geolocateme')
-    geolocateMe() 
-  })
 
 
-  
+  // calculates distance between two points in km's
+//   function calcDistance(p1, p2) {
+    
 
-
-var p1 = new google.maps.LatLng(47.380366, -3.671338); 
-var p2 = new google.maps.LatLng(40.383036, -3.674160); 
-
-console.log(calcDistance(p1, p2)); 
-
-//calculates distance between two points in km's 
-function calcDistance(p1, p2) { 
-    return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2)/1000).toFixed(2); 
-} 
-
-  
-  
+//     return (
+//       google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000
+//     ).toFixed(2);
+//   }
+});
